7065554beaf1d0d0407941d4409aacfd1bc519d4
pmita <panos.mita2@outlook.com>
2022-06-11 22:29:15 +0100

    notes on components and props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 //importing useState hook from the react library
-import { useState } from 'react'; 
+import React, { useState } from 'react'; 
+//COMPONENTS
+import Title from './components/Title';
+import Modal from './components/Modal';
 import './App.css';
 
 function App() {
   //STATE & VARIALBES
+  const [showModal, setShowModal] = useState(true);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     {title: 'mario birthdat is bash', id: 1},
@@ -18,21 +22,31 @@ function App() {
     })
   }
 
+  //FUNCTIONS
+  const handleClose = () => {
+    setShowModal(false)
+  }
 
 
   return (
     <div className='App'>
+      <Title title='Mario Kingdom Events' subtitle='Random text here' />
 
       <div>
         <button onClick={() => setShowEvents(false)}>Hide Events</button>
         <button onClick={() => setShowEvents(true)}>Show Evenets</button>
       </div>
       {showEvents && events.map((event, index) => (
-        <div key={event.id}>
+        <React.Fragment key={event.id}>
           <h2>{index} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>Delete Item</button>
-        </div>
+        </React.Fragment>
       ))}
+
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>10% off code</h2>
+        <p>Use Panos at checkout</p>
+      </Modal>}
     </div>
   );
 }
